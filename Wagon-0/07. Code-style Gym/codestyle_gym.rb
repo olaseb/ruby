1918382a23ab6cfd48f5d46b9856fafab64d096a
ola <ola.seb@hotmail.fr>
2014-01-07 14:13:23 +0100

    Exercice 7

--- Wagon-0/07. Code-style Gym/codestyle_gym.rb
+++ Wagon-0/07. Code-style Gym/codestyle_gym.rb
@@ -1,46 +1,52 @@
 class Player_moving
   Grid_POSITIONS = (1..100).to_a
   
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
-raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
+  def initialize ( position ) 
+    @position = position
+    @iterations = 0; 
+      raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
   end
   
-      def move   distanceInMeters
-@position += distanceInMeters
+  def move distanceInMeters
+    @position += distanceInMeters
   end
   
-def play
-  if(  self.status_Player==:play   ) 
-if rand(1..100) < 50 ; move(rand(1..10))
-  else
+  def play
+    if (self.status_Player == :play) 
+    elsif rand(1..100) < 50
+       move(rand(1..10))
+    else
         move(-rand(1..10))
-end
+    end
   end
-end
 
-def cheat; @position = 101; end
+  def cheat
+     @position = 101
+  end
   
-def status_Player
-    if( @position >   100 )
+  def status_Player
+    if (@position >   100)
       :win 
-    elsif(@position<0) ; :loose
-    else;  :play
+    elsif (@position<0)
+      :loose
+    else
+      :play
     end
   end
   
-  def hasWon ; status_Player  == :win ; end
-    
-end
+  def hasWon 
+    status_Player  == :win 
+  end
+end   
 
-player = Player_moving.new(50)
+player = Player_moving.new (50)
 player.play
 puts player.status_Player == :play # true
 player.cheat
 puts player.hasWon == true
   
 begin
-  Player_moving.new(110)
-rescue RuntimeError => e
-  puts e.to_s == "Position must be between 1 and 100" # true
+  Player_moving.new (110)
+    rescue RuntimeError => e
+      puts e.to_s == "Position must be between 1 and 100" # true
 end
