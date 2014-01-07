90fb29e047643b5738e4a589ab332a9b576d00ef
ola <ola.seb@hotmail.fr>
2014-01-07 16:08:14 +0100

    Exercice 1_1

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,12 +1,30 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
-
 def sum(min,max)
-  # your code here
+  res = 0
+  i = min
+  
+    while i <= max
+      res += i
+      i += 1
+    end
+    res
 end
+
+def sum2(min,max)
+  res = 0
+  i = min
+  for i in min ..max
+    res += i
+  end
+  res
+end
+
 
 # Testing your code
 
 min = 1
 max = 100
 sum = sum(1,100)
-puts sum == 5050 # => true
+sum2 = sum2(1,100)
+puts(sum == 5050) # => true
+puts(sum2 == 5050) # => true
