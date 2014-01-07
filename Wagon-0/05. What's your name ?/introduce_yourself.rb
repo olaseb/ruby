3efb78028cc19ad4a9522ec28c292ce887c1131a
ola <ola.seb@hotmail.fr>
2014-01-07 12:05:32 +0100

    Exercice 06

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,15 @@
 # your code goes here, introduce yourself !
+
+def introduce(name, firstname)
+  name = gets.chomp
+  firstname = gets.champ
+  puts "Hello #{name + firstname} !"
+end
+
+
+def introduce(name, firstname)
+  name = gets.chomp
+  firstname = gets.champ
+  puts 'Hello #{name}' + '{firstname} !'
+/bin/bash: puts: command not found
+end
