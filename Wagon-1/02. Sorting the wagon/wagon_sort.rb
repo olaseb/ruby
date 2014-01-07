fbff503b679e8b57f146689880389110a88de204
ola <ola.seb@hotmail.fr>
2014-01-07 17:01:30 +0100

    Exercice 1_2

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,22 @@
 def wagon_sort( students_array )
   # your code to sort the array and output students
+  students_array.sort!
+    for name in students_array
+      puts name
+    end
 end
 
+puts "what's your name?"
+name1 = gets.chomp
+array = [name1]
+
+puts "what's your name?"
+name2 = gets.chomp
+array << name2
+
+puts "what's your name?"
+name3 = gets.chomp
+array << name3
+
+wagon_sort(array)
 # your code to interact with the user..
