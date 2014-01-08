31a1b1571755d16f557d9f224fc89cc4e485c827
ola <ola.seb@hotmail.fr>
2014-01-08 18:42:36 +0100

    days 3

--- Wagon-1/03. Beer song /beer_song.rb
+++ Wagon-1/03. Beer song /beer_song.rb
@@ -1,18 +1,19 @@
 def beersong(nb_at_start)
-  # your code goes here
+    # => premier couplet
+    puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+        x = nb_at_start - 1
+    puts "Take one down, pass it around, #{x} bottles of beer on the wall!"
+
+    # => a partir du deuxieme couplet    
+    until x == 2
+        puts "#{x} bottles of beer on the wall, #{x} bottles of beer!"
+        x -= 1
+        puts "Take one down, pass it around, #{x} bottles of beer on the wall!"
+    end
+
+    puts "2 bottles of beer on the wall, 2 bottles of beer!"
+    puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+    puts "1 bottle of beer on the wall, 1 bottle of beer!"
+  puts "Take one down, pass it around, no more bottles of beer on the wall!"
+
 end
-
-beersong( ARGV[0] )
-
-# output (if called with 5 beers) :
-
-# => 5 bottles of beer on the wall, 5 bottles of beer!
-# => Take one down, pass it around, 4 bottles of beer on the wall!
-# => 4 bottles of beer on the wall, 4 bottles of beer!
-# => Take one down, pass it around, 3 bottles of beer on the wall!
-# => 3 bottles of beer on the wall, 3 bottles of beer!
-# => Take one down, pass it around, 2 bottles of beer on the wall!
-# => 2 bottles of beer on the wall, 2 bottles of beer!
-# => Take one down, pass it around, 1 bottle of beer on the wall!
-# => 1 bottle of beer on the wall, 1 bottle of beer!
-# => Take one down, pass it around, no more bottles of beer on the wall!
--- Wagon-1/05. Roman numerals/roman_numerals.rb
+++ Wagon-1/05. Roman numerals/roman_numerals.rb
@@ -1,10 +1,37 @@
 def old_roman_numeral(an_integer)
-  # your code goese here
+  nb_m = an_integer.divmod(1000)
+  m = nb_m[0]
+  print "M" * m
+  
+  nb_c = nb_m[1].divmod(100)
+  c = nb_c[0]
+  print "C" * c
+  
+  nb_d = nb_c[1].divmod(10)
+  d = nb_d[0]
+  print "X" * d
+  
+  nb_u = nb_d[1].divmod(1)
+  u = nb_u[0]
+  puts "I" * u
 end
 
+old_roman_numeral(3536)
+
+=begin
 def roman_numeral(an_integer)
-  # your code goese here for the new-style version
+  # your code goese here anor the new-style version
 end
 
 puts "My nice roman numeral tests" 
 # Write a nice testing script herebelow !
+
+
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+=end
