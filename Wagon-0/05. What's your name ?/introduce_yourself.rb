4905c695fc0a283f2f449d44dda25b17df418909
ola <ola.seb@hotmail.fr>
2014-01-10 18:42:41 +0100

    Exercice What's your name? en 4 Méthodes

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,15 +1,70 @@
-# your code goes here, introduce yourself !
+############ Methode 1 ############ 
 
-def introduce(name, firstname)
-  name = gets.chomp
-  firstname = gets.champ
-  puts "Hello #{name + firstname} !"
-end
+# puts "What's your first name?"
+# first_name = gets.chomp
+# 
+# puts "What's your second name?"
+# second_name = gets.chomp
+# 
+# puts "Welcome #{first_name} #{second_name}"
+
+############ ############ ############
 
 
-def introduce(name, firstname)
-  name = gets.chomp
-  firstname = gets.champ
-  puts 'Hello #{name}' + '{firstname} !'
-/bin/bash: puts: command not found
-end
+
+################# Methode 2 ################# 
+
+# puts "What's your first name?"
+# first_name = gets.chomp
+# puts "What's second name?"
+# second_name = gets.chomp
+# puts "Welcome #{first_name} " + second_name
+
+################# ################# ################# 
+
+
+
+
+########################## Methode 3 ##########################
+
+# def interesting(response)
+#   if response == "yes"
+#     answer = "cool!!!"
+#   else
+#     answer = "boring....."
+#   end
+#  return answer 
+# end
+# 
+# puts "What is your job?"
+# job = gets.chomp
+# puts "It is a interesting job?"
+# interesting_job = gets.chomp
+# puts "So your job is #{job} and #{interesting(interesting_job)}"
+
+########################## ########################## ##########################
+
+
+
+############################################# Methode 4 ############################################# 
+
+# def welcome
+#   array = []
+#   puts "What's your first name?"
+#   first_name = gets.chomp
+#   array << first_name
+# 
+#   puts "What's your second name?"
+#   second_name = gets.chomp
+#   array << second_name
+#   
+#   array
+# 
+# end
+# 
+# array = welcome
+# puts "Welcome LeWagon #{array[0]} #{array[1]}"
+
+############################################# ############################################# ############################################# 
+
+
